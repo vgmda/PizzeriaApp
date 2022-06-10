0f3dcb3ae9e0800fe4bdaa17131e2867bea828bb
vgmda <vasilegancin@gmail.com>
2022-06-10 22:34:37 +0100

    Defining proprieties

--- PizzeriaApp/Models/PizzasModel.cs
+++ PizzeriaApp/Models/PizzasModel.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 namespace PizzeriaApp.Models;
 
+/// <summary>
+/// Pizza model hold the data for all of our ingredients.
+/// </summary>
+
 public class PizzasModel
 {
-    public PizzasModel()
-    {
-    }
+    public string ImageTitle { get; set; }
+
+    public string PizzaName { get; set; }
+
+    public float BasePrice { get; set; }
+
+    public bool TomatoSauce { get; set; }
+
+    public bool Pepperoni { get; set; }
+
+    public bool Mushroom { get; set; }
+
+    public bool Tuna { get; set; }
+
+    public bool Pineapple { get; set; }
+
+    public bool Ham { get; set; }
+
+    public bool Beef { get; set; }
+
 }
 
