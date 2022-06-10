e0e82b29032fcd514c24591c58da3fae8b548483
vgmda <vasilegancin@gmail.com>
2022-06-10 23:20:57 +0100

    Redirect the data

--- PizzeriaApp/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzeriaApp/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PizzeriaApp.Models;
@@ -15,8 +12,30 @@ namespace PizzeriaApp.Pages.Forms
         // Create reference to the actual PizzasModel
         public PizzasModel Pizza { get; set; }
 
+        public float PizzaPrice { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce)
+            {
+                PizzaPrice += 1;
+            }
+
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Pepperoni) PizzaPrice += 1;
+            if (Pizza.Mushroom) PizzaPrice += 1;
+            if (Pizza.Tuna) PizzaPrice += 1;
+            if (Pizza.Pineapple) PizzaPrice += 10;
+            if (Pizza.Ham) PizzaPrice += 1;
+            if (Pizza.Beef) PizzaPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
         }
     }
 }
