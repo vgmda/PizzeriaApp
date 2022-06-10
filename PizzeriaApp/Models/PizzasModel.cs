67caaa973c6647662c09fd952cd8f876f7ac5629
vgmda <vasilegancin@gmail.com>
2022-06-10 22:54:55 +0100

    Design CustomPizza html

--- PizzeriaApp/Models/PizzasModel.cs
+++ PizzeriaApp/Models/PizzasModel.cs
@@ -17,6 +17,8 @@ public class PizzasModel
 
     public bool Pepperoni { get; set; }
 
+    public bool Cheese { get; set; }
+
     public bool Mushroom { get; set; }
 
     public bool Tuna { get; set; }
@@ -26,6 +28,8 @@ public class PizzasModel
     public bool Ham { get; set; }
 
     public bool Beef { get; set; }
+
+    public float FinalPrice { get; set; }
 
 }
 
--- PizzeriaApp/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzeriaApp/Pages/Forms/CustomPizza.cshtml.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzeriaApp.Models;
 
 namespace PizzeriaApp.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+
+        [BindProperty]
+        // Create reference to the actual PizzasModel
+        public PizzasModel Pizza { get; set; }
+
         public void OnGet()
         {
         }
