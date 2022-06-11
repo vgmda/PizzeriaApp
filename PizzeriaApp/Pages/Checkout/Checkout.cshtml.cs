83b13e86cf28ecafee0650cb0b5ee8ecbe0289ca
vgmda <vasilegancin@gmail.com>
2022-06-11 22:40:02 +0100

    Working on Checkout page

--- PizzeriaApp/Pages/Checkout/Checkout.cshtml.cs
+++ PizzeriaApp/Pages/Checkout/Checkout.cshtml.cs
@@ -7,10 +7,28 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PizzeriaApp.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
+
     public class CheckoutModel : PageModel
     {
+
+        public string PizzaName { get; set; }
+
+        public float PizzaPrice { get; set; }
+
+        public string ImageTitle { get; set; }
+
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
         }
     }
 }
