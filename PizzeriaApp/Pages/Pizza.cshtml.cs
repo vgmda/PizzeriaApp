1cca2fadbdbe6aedb42dceb5032a58b05f86f25d
vgmda <vasilegancin@gmail.com>
2022-06-12 23:21:53 +0100

    Final adjustments

--- PizzeriaApp/Pages/Pizza.cshtml.cs
+++ PizzeriaApp/Pages/Pizza.cshtml.cs
@@ -13,15 +13,95 @@ namespace PizzeriaApp.Pages.Shared
         public List<PizzasModel> fakePizzaDb = new List<PizzasModel>()
         {
             new PizzasModel(){
-                ImageTitle="Margherita",
-                PizzaName="Margherita",
-                BasePrice=2,
-                TomatoSauce=true,
-                Cheese=true}
+                ImageTitle = "Margherita",
+                PizzaName = "Margherita",
+                BasePrice = 2,
+                TomatoSauce = true,
+                Cheese = true,
+                FinalPrice = 4},
+
+            new PizzasModel(){
+                ImageTitle = "Hawaiian",
+                PizzaName = "Hawaiian",
+                BasePrice = 2,
+                TomatoSauce = true,
+                Cheese = true,
+                Ham = true,
+                Pineapple = true,
+                FinalPrice = 5},
+
+            new PizzasModel(){
+                ImageTitle = "Bolognese",
+                PizzaName = "Bolognese",
+                BasePrice = 2,
+                TomatoSauce = true,
+                Cheese = true,
+                Beef = true,
+                FinalPrice = 5},
+
+
+            new PizzasModel(){
+                ImageTitle = "Carbonara",
+                PizzaName = "Carbonara",
+                BasePrice = 2,
+                TomatoSauce = true,
+                Cheese = true,
+                Ham = true,
+                Mushroom = true,
+                FinalPrice = 8},
+
+
+            new PizzasModel(){
+                ImageTitle = "MeatFeast",
+                PizzaName = "MeatFeast",
+                BasePrice = 2,
+                TomatoSauce = true,
+                Cheese = true,
+                Ham = true,
+                Beef = true,
+                FinalPrice = 10},
+
+            new PizzasModel(){
+                ImageTitle = "Mushroom",
+                PizzaName = "Mushroom",
+                BasePrice = 2,
+                TomatoSauce = true,
+                Cheese = true,
+                Mushroom = true,
+                FinalPrice = 5},
+
+            new PizzasModel(){
+                ImageTitle = "Pepperoni",
+                PizzaName = "Pepperoni",
+                BasePrice = 2,
+                TomatoSauce = true,
+                Cheese = true,
+                Pepperoni = true,
+                FinalPrice = 6},
+
+            new PizzasModel(){
+                ImageTitle = "Seafood",
+                PizzaName = "Seafood",
+                BasePrice = 2,
+                TomatoSauce = true,
+                Cheese = true,
+                Tuna = true,
+                FinalPrice = 6},
+
+            new PizzasModel(){
+                ImageTitle = "Vegetarian",
+                PizzaName = "Vegetarian",
+                BasePrice = 2,
+                TomatoSauce = true,
+                Cheese = true,
+                Mushroom = true,
+                Pineapple = true,
+                FinalPrice = 7}
         };
 
         public void OnGet()
         {
+
         }
     }
 }
